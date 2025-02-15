d6efea0118562421ce195de9fe1575a126693004
alvaradodaniel <alvaradodaniel128@gmail.com>
2025-02-15 13:41:04 -0600

    :alembic: Experimentando

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
-//import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
