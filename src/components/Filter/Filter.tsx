95a5fc67daf429c8682aeeb441ab1351acf65506
TheSixDecadence <DelosSantosJ.10.3A@gmail.com>
2025-02-15 14:55:19 -0600

    Filter Button

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Filter.css";
+
+export interface FilterProps {
+    label: string;
+    isActive?: boolean;
+}
+
+const Filter = ({ label, isActive = false }: FilterProps) => {
+    const buttonClasses = `filter-button ${isActive ? 'filter-button-active' : ''}`;
+    
+    return (
+        <button className={buttonClasses}>
+            {isActive && '✔'} {label}
+        </button>
+    );
+}
+
+export default Filter;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from "./Button/index";
 export { default as Lection } from "./Lection/index";
+export * from "./Filter";
