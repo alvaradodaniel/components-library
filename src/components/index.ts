fd9e18c29e97d362d1c7f2d5d093f557d74a2660
alvaradodaniel <alvaradodaniel128@gmail.com>
2025-02-15 14:46:05 -0600

    cambiar export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as OptionNav } from "./OptionNav/index"
-export { default as Course } from "./Course/index"
-export { default as Lection } from "./Lection/index"
+export * from "./OptionNav";
+export * from "./Course";
+export * from "./Lection";
